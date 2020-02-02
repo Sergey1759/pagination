6c722ecaf2175cc53b94738d6c623ed77167ae86
Sergey1759 <lisichka1759@gmail.com>
2020-02-02 02:39:05 +0200

    need refactoring

--- js.js
+++ js.js
@@ -5,19 +5,20 @@ function getData(){
 
 async function data(){
     const res = await getData();
-    console.log(res)
     addDOM(res);
-
 }
+let max_kil = 8;
+let active = [];
+let active_pag;
 function addDOM(res){
-    console.log(res)
-    let active = []
+    //console.log(res)
     let conteiner = document.getElementsByClassName('conteiner')[0];
     for(let i = 0; i < res.length;i++){
         let newElement = document.createElement('div');
         newElement.classList.add('card');
         newElement.id = res[i].id;
-        if (i <= 19){
+        if (i < max_kil){
+            newElement.classList.add('show')
             active.push(newElement)
         } else{
             newElement.classList.add('hide')
@@ -25,36 +26,50 @@ function addDOM(res){
         newElement.innerHTML = res[i].title;
         conteiner.appendChild(newElement);
     }
-}
-data()
-
-let but = document.getElementsByClassName('pag');
-
+    let pagination = document.getElementsByClassName('pagination')[0];
+    for(let i = 0; i < Math.ceil(res.length / max_kil); i++){
+        let pag = document.createElement('div');
+        pag.classList.add('pag');
+        if(i == 0) pag.classList.add('active_pag');
+        pag.id = i+1;
+        pag.innerHTML = i+1;
+        pagination.appendChild(pag);
+    }
+    let but = document.getElementsByClassName('pag');
+    let active_pag = document.getElementsByClassName('active_pag')[0];
 for(let i = 0; i < but.length; i++){
     but[i].addEventListener('click',function(){
         clearAll();
-        show(20,this.id)
+        show(max_kil,but[i].id);
+        active_pag.classList.remove('active_pag');
+        but[i].classList.add('active_pag');
+        active_pag = but[i];
     },false)
 }
+}
+data()
+
+
 function clearAll(){
-    let card = document.getElementsByClassName('card');
+    let card = active;
     for(let i = 0; i < card.length; i++) {
-            card[i].classList.add('hide');
             card[i].classList.remove('show');
+            card[i].classList.add('hide');
     };
 }
 function show(kil, id){
     kil = parseInt(kil)
     id = parseInt(id)
     let card = document.getElementsByClassName('card');
+    active.length = 0;
     for(let i = 0; i < card.length; i++) {
-        let min = (id-1)*kil;
+        let min = ((id-1)*kil) + 1;
         let max = ((id)*kil);
         if(card[i].id >= min && card[i].id <= max){
             console.log("min " + min + "   max " + max + "  i =" + i)
-
             card[i].classList.add('show');
             card[i].classList.remove('hide');
+            active.push(card[i]);
         }
         
     };
