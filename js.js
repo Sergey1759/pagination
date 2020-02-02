81d583ac4809216ae71723776d5ca94a5388da89
Sergey1759 <lisichka1759@gmail.com>
2020-02-02 03:54:52 +0200

    add animation

--- js.js
+++ js.js
@@ -40,7 +40,10 @@ function addDOM(res){
 for(let i = 0; i < but.length; i++){
     but[i].addEventListener('click',function(){
         clearAll();
-        show(max_kil,but[i].id);
+            setTimeout(() =>{
+                show(max_kil,but[i].id);
+            },800); 
+        
         active_pag.classList.remove('active_pag');
         but[i].classList.add('active_pag');
         active_pag = but[i];
@@ -52,23 +55,38 @@ data()
 
 function clearAll(){
     let card = active;
+    card.reverse();
     for(let i = 0; i < card.length; i++) {
-            card[i].classList.remove('show');
-            card[i].classList.add('hide');
+            setTimeout(() =>{
+                card[i].classList.add('hide_animation');
+                setTimeout(() =>{
+                    card[i].classList.remove('show');
+                    card[i].classList.add('hide');
+                },95)
+            },i*100)   
     };
 }
 function show(kil, id){
     kil = parseInt(kil)
     id = parseInt(id)
+    
     let card = document.getElementsByClassName('card');
     active.length = 0;
+    let counter = 0;
     for(let i = 0; i < card.length; i++) {
         let min = ((id-1)*kil) + 1;
         let max = ((id)*kil);
         if(card[i].id >= min && card[i].id <= max){
-            console.log("min " + min + "   max " + max + "  i =" + i)
-            card[i].classList.add('show');
-            card[i].classList.remove('hide');
+           // console.log("min " + min + "   max " + max + "  i =" + i)
+            setTimeout(() =>{
+                card[i].classList.add('show_animation');
+                setTimeout(() =>{
+                    console.log('show');
+                    card[i].classList.add('show');
+                    card[i].classList.remove('hide');
+                },90)
+            },counter*100);
+            counter++;
             active.push(card[i]);
         }
         
